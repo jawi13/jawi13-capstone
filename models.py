7d2a974368bae767889b49c11effd420dca4eaa6
James <jawi13@outlook.com>
2020-05-27 17:17:53 +0100

    try to fix connection

--- models.py
+++ models.py
@@ -1,18 +1,16 @@
 import os
 from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
-
-database_name = "capstone"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
-def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+def setup_db(app):
+    app.config.from_object('config')
     db.app = app
     db.init_app(app)
     db.create_all()
+    migrate = Migrate(app, db)
 
 class Actor(db.Model):
     __tablename__ = 'actor'
--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@ export AUTH0_DOMAIN ="jawi13.eu.auth0.com"
 export API_AUDIENCE="capstone"
 export ALGORITHMS=["RS256"]
 export DATABASE_URL="postgres://grjpvsmpqjhecy:3ee69ebf1c9bf503d0982108b5fc6e0da6a86b8443875ba5405ef37caceadcdc@ec2-34-198-243-120.compute-1.amazonaws.com:5432/dbfmt3uid29nfi"
+export FLASK_APP=app.py
+export FLASK_ENV=development
+export SQLALCHEMY_DATABASE_URI="postgres://grjpvsmpqjhecy:3ee69ebf1c9bf503d0982108b5fc6e0da6a86b8443875ba5405ef37caceadcdc@ec2-34-198-243-120.compute-1.amazonaws.com:5432/dbfmt3uid29nfi"
